202222676296eca2d5c1ea439aeb1b061422d821
VaishaliAmdocs <75739549+VaishaliAmdocs@users.noreply.github.com>
2020-12-10 16:26:09 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,9 @@
 a = 33
 b = 33
-if b > a:
-    print("b is greater than a")
-elif a == b:
-    print("a and b are equal")
-if b > a :
-    do_x()
+if b > a:    
     assert True, ("correct")
-elif a == b :
-    do_y(), ("Correct")
+elif a == b:
+    assert True, ("Correct")
 else:
     assert False, ("This should never happen, but it does occasionally. "
                    "We're currently trying to figure out why. "
