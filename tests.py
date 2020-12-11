2c1a331dbd73b798e479f3e49ab931a73e0bf380
VaishaliAmdocs <75739549+VaishaliAmdocs@users.noreply.github.com>
2020-12-11 15:26:51 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 a = 33
 b = 33
 if b > a:
-    assert True, ("correct")
+    assert True, ("correct1")
 elif a == b:
-    assert True, ("Correct")
+    assert True, ("Correct2")
 else:
     assert False, ("This should never happen, but it does occasionally. "
                    "We're currently trying to figure out why. "
