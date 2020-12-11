8d6bc4e0541d19beb1cbeca8a9c38a8f95427f25
VaishaliAmdocs <75739549+VaishaliAmdocs@users.noreply.github.com>
2020-12-11 16:10:09 +0200

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,20 +1,20 @@
 def max(values):
 
-  _max = values[0]
+    _max = values[0]
 
-  for val in values:
-      if val > _max:
-          _max = val
+    for val in values:
+        if val > _max:
+            _max = val
 
-  return _max
+    return _max
 
 
 def min(values):
 
-  _min = values[0]
+    _min = values[0]
 
-  for val in values:
-      if val < _min:
-          _min = val
+    for val in values:
+        if val < _min:
+            _min = val
 
-  return _min
+    return _min
