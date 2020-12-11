c80f1059b6dd00a8dbb295abbb5136939fd2550f
VaishaliAmdocs <75739549+VaishaliAmdocs@users.noreply.github.com>
2020-12-11 16:14:53 +0200

    Update min_max_test.py

--- min_max_test.py
+++ min_max_test.py
@@ -1,14 +1,12 @@
 import algo
 
-    def test_min(): 
-    
+    def test_min():    
     values = (2, 3, 1, 4, 6)
 
     val = algo.min(values)
     assert val == 1
 
     def test_max():
-        
     values = (2, 3, 1, 4, 6)
 
     val = algo.max(values)
